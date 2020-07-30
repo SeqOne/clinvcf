e26ad4027dcfba90678f8f4d5edb57732cd74389
Sacha Beaumeunier <sacha.beaumeunier@cortect.com>
2020-07-30 16:53:36 +0200

    fix(toy): :white_check_mark: #20 reduce toy size

--- tests/hgnc_tests.nim
+++ tests/hgnc_tests.nim
@@ -5,10 +5,7 @@ import clinvcfpkg/hgnc
 suite "test HGNC functions":
 
   test "test initHgncDbfromFile":
-    var f = "tests/files/hgnc_entrez.tsv"
+    var f = "tests/files/hgnc_toy.tsv"
     var hgncIndex = initHgncDbfromFile(f)
     # assert alias give the same entrez ID
-    check hgncIndex.alias["PPP1R53"] == "BRCA1"
-    check hgncIndex.alias["KIAA0644"] == "TRIL"
-    check hgncIndex.alias["DBA"] == "RPS19"
     check hgncIndex.alias["LFS1"] == "TP53"
